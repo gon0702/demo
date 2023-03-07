b473f02ac69d041082be208fb78745e91c63ee54
gon0702 <corph@naver.com>
2023-03-07 19:32:21 +0900

    test

---
+++ src/main/java/com/example/demo/rest/controller/RestTestController.java
@@ -0,0 +1,24 @@
+package com.example.demo.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class RestTestController {
+    @GetMapping
+    public String getController(){
+        return "GET - /test";
+    }
+    @PostMapping
+    public String postController(){
+        return "POST - /test";
+    }
+
+    @GetMapping("/world")
+    public String getWorld(){
+        return "Hello World!";
+    }
+}
